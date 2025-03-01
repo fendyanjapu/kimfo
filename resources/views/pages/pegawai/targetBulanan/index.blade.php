dc3b8366f963bbbb7e6aba21908b2823e0b88469
Noor R. Effendy <fendy.anjapu@gmail.com>
2025-03-01 11:51:56 +0800

    fix target bulanan

--- resources/views/pages/pegawai/targetBulanan/index.blade.php
+++ resources/views/pages/pegawai/targetBulanan/index.blade.php
@@ -1,26 +1,26 @@
 @extends('layouts.template')
 
 @section('content')
-<script>
-  $(document).ready(function(){
-    $('#tabel').DataTable( {
-        scrollX: true,
-        scrollY: true,
-       "language": {
-            "url": "//cdn.datatables.net/plug-ins/1.10.21/i18n/Indonesian.json"
-        }
-    } );
-  });
-</script>
-<h2>
+  <script>
+    $(document).ready(function () {
+    $('#tabel').DataTable({
+      scrollX: true,
+      scrollY: true,
+      "language": {
+      "url": "//cdn.datatables.net/plug-ins/1.10.21/i18n/Indonesian.json"
+      }
+    });
+    });
+  </script>
+  <h2>
     <div class="par-text">Target Bulanan</div>
     <div class="par-tex2">
-</h2><br>
-<a href="{{ route('targetBulanan.create') }}"
-      class="btn btn-primary mb-4" title="Tambah"><i class="icon-plus me-1"></i>Tambah</i></a>
-<table id="tabel" class="table table-striped table-bordered">
+  </h2><br>
+  <a href="{{ route('targetBulanan.create') }}" class="btn btn-primary mb-4" title="Tambah"><i
+    class="icon-plus me-1"></i>Tambah</i></a>
+  <table id="tabel" class="table table-striped table-bordered">
     <thead>
-     <tr>
+    <tr>
       <th style="vertical-align: middle; text-align: center" width="15px">No</th>
       <th style="vertical-align: middle; text-align: center" width="15px">#</th>
       <th style="vertical-align: middle; text-align: center">Indikator</th>
@@ -38,38 +38,40 @@
       <th style="vertical-align: middle; text-align: center">Oktober</th>
       <th style="vertical-align: middle; text-align: center">November</th>
       <th style="vertical-align: middle; text-align: center">Desember</th>
-     </tr>
-  </thead>
-  <tbody>
+    </tr>
+    </thead>
+    <tbody>
     @foreach ($targetBulanans as $item)
     <tr>
-        <td style="text-align: center;width:1%">{{ $loop->iteration }}</td>
-        <td style="text-align: center;">{{ $item->indikator?->nama_indikator }}</td>
-        <td style="text-align: center">
-          <a href="{{ route('targetBulanan.edit', ['targetBulanan' => $item]) }}" class="btn btn-success btn-sm mt-1" title="edit"><i class="icon-pencil"></i></a>
+      <td style="text-align: center;width:1%">{{ $loop->iteration }}</td>
+      <td style="text-align: center">
+      <a href="{{ route('targetBulanan.edit', ['targetBulanan' => $item]) }}" class="btn btn-success btn-sm mt-1"
+      title="edit"><i class="icon-pencil"></i></a>
 
-          {{-- <a href="{{ route('targetBulanan.destroy', $item->id) }}" class="btn btn-danger btn-sm mt-1" data-confirm-delete="true">
-            <i class="icon-trash"></i> --}}
-        </a>
+      {{-- <a href="{{ route('targetBulanan.destroy', $item->id) }}" class="btn btn-danger btn-sm mt-1"
+      data-confirm-delete="true">
+      <i class="icon-trash"></i> --}}
+      </a>
       </td>
-        <td style="text-align: center;">{{ $item->indikator?->target }}</td>
-        <td style="text-align: center;">{{ $item->indikator?->satuan }}</td>
-        <td style="text-align: center;">{{ $item->jan }}</td>
-        <td style="text-align: center;">{{ $item->feb }}</td>
-        <td style="text-align: center;">{{ $item->mar }}</td>
-        <td style="text-align: center;">{{ $item->apr }}</td>
-        <td style="text-align: center;">{{ $item->mei }}</td>
-        <td style="text-align: center;">{{ $item->jun }}</td>
-        <td style="text-align: center;">{{ $item->jul }}</td>
-        <td style="text-align: center;">{{ $item->agu }}</td>
-        <td style="text-align: center;">{{ $item->sep }}</td>
-        <td style="text-align: center;">{{ $item->okt }}</td>
-        <td style="text-align: center;">{{ $item->nov }}</td>
-        <td style="text-align: center;">{{ $item->des }}</td>
-        
-      </tr>
-    @endforeach
-  </tbody>
-</table>
+      <td style="text-align: center;">{{ $item->indikator?->nama_indikator }}</td>
+      <td style="text-align: center;">{{ $item->indikator?->target }}</td>
+      <td style="text-align: center;">{{ $item->indikator?->satuan }}</td>
+      <td style="text-align: center;">{{ $item->jan }}</td>
+      <td style="text-align: center;">{{ $item->feb }}</td>
+      <td style="text-align: center;">{{ $item->mar }}</td>
+      <td style="text-align: center;">{{ $item->apr }}</td>
+      <td style="text-align: center;">{{ $item->mei }}</td>
+      <td style="text-align: center;">{{ $item->jun }}</td>
+      <td style="text-align: center;">{{ $item->jul }}</td>
+      <td style="text-align: center;">{{ $item->agu }}</td>
+      <td style="text-align: center;">{{ $item->sep }}</td>
+      <td style="text-align: center;">{{ $item->okt }}</td>
+      <td style="text-align: center;">{{ $item->nov }}</td>
+      <td style="text-align: center;">{{ $item->des }}</td>
+
+    </tr>
+  @endforeach
+    </tbody>
+  </table>
 
 @endsection
